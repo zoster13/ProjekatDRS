f7376ac130f0bf6e493a54ac102e3e0f89f4bc49
Aleksandra Misic <alexmisic94@gmail.com>
2016-12-27 21:59:39 +0100

    Napravljen tab sa podesavanjima

--- Moduo2/Server/Data/Employee.cs
+++ Moduo2/Server/Data/Employee.cs
@@ -4,26 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HiringCompany
+namespace Server.Data
 {
-    public enum EmployeeType
-    {
-        CEO = 0,
-        ENG,        //engineer
-        TL,         //team leader
-        SM          // scrum master
-    }
-
     public class Employee
     {
-        private string username;
-        private string password;
         private EmployeeType type;
         private string name;
         private string surname;
         private string email;
+        private string password;
 
-        private Employee() 
+
+        private Employee()
         {
 
         }
