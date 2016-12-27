91ac43429b9d180cc26814486cbebafcae3886bf
Zvezdana Klasnic <zklasnic94@gmail.com>
2016-12-27 14:00:58 +0100

    Dodala klasu Employee

---
+++ Moduo1/HiringCompany/Data/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringCompany
+{
+    public enum EmployeeType
+    {
+        CEO=0,
+        PO,
+        SM,
+        HR
+    }
+
+    public class Employee
+    {
+        private EmployeeType type;
+        private string name;
+        private string surname;
+        private string email;
+        private string password;
+
+
+        private Employee() 
+        {
+
+        }
+    }
+}
