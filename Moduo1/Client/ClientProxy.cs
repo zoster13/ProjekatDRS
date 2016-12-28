ded73daf9e946ce2d961ed2086de055bc7336be5
Miljana Vujakovic <miljana_lo@hotmail.com>
2016-12-28 00:57:40 -1100

    Modul1-clientproxy

--- Moduo1/Client/ClientProxy.cs
+++ Moduo1/Client/ClientProxy.cs
@@ -4,10 +4,163 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 namespace Client
 {
-    public class ClientProxy
+    public class ClientProxy : DuplexClientBase<EmployeeCommon.IEmployeeService>, EmployeeCommon.IEmployeeService,IDisposable
     {
+        EmployeeCommon.IEmployeeService factory;
+
+        public ClientProxy(InstanceContext callbackContext, Binding binding, EndpointAddress remoteAddress) :
+         base(callbackContext, binding, remoteAddress)
+        {
+            factory = this.ChannelFactory.CreateChannel();           
+        }
+
+
+
+        public void SignIn()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                
+               
+            }
+        }
+
+        public void SignOut()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void ListOnlineEmployees()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void ListOutsorcingCompanies()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void ChangeEmployeeData()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void SetWorkingHours()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void AskForPartnership()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void AddNewEmployee()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void ChangeEmployeeType()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void ProjectOverview()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        public void CreateNewProject()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
     }
 }
--- Moduo1/EmployeeCommon/EmployeeType.cs
+++ Moduo1/EmployeeCommon/EmployeeType.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeCommon
 {
+    [DataContract]
     public enum EmployeeType
     {
+        [EnumMember]
         CEO = 0,
+        [EnumMember]
         PO,
+        [EnumMember]
         HR,
+        [EnumMember]
         SM
     }
 }
