51ea29675bd3a3b2495916e5b88fb72a4d4dd5e0
Zvezdana Klasnic <zklasnic94@gmail.com>
2017-01-09 09:21:20 +0100

    ICommon-dodate klase Project i UserStory

---
+++ Common/ICommon/ProjectCommon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ICommon
+{
+    [DataContract]
+    public class ProjectCommon
+    {
+        private string name;
+        private string description;
+        private DateTime startDate;
+        private DateTime deadline;
+
+        public ProjectCommon() 
+        {
+            
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        [DataMember]
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        [DataMember]
+        public DateTime Deadline
+        {
+            get { return deadline; }
+            set { deadline = value; }
+        }
+    }
+}
---
+++ Common/ICommon/UserStoryCommon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommon
+{
+    [DataContract]
+    public class UserStoryCommon
+    {
+        private string title;
+        private string description;
+        private string acceptanceCriteria;
+        private bool isAccepted;
+
+        public UserStoryCommon() 
+        {
+        }
+
+        [DataMember]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        [DataMember]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        [DataMember]
+        public string AcceptanceCriteria
+        {
+            get { return acceptanceCriteria; }
+            set { acceptanceCriteria = value; }
+        }
+
+        [DataMember]
+        public bool IsAccepted 
+        {
+            get { return isAccepted; }
+            set { isAccepted = value; }
+        }
+    }
+}
