8f64d70aa39f75d28d0980d6fed9b69fa39cd0fb
Miljana Vujakovic <miljana_lo@hotmail.com>
2017-01-11 21:42:08 -1100

    moduo1

--- Moduo1/HiringCompany/Notifier.cs
+++ Moduo1/HiringCompany/Notifier.cs
@@ -58,10 +58,9 @@ namespace HiringCompany
 
             messageToLog.AppendLine("finished successfully");
             Program.Logger.Info(messageToLog);
-
         }
 
-        public void SyncSpecialClients( EmployeeType type, string notification )
+        public void SyncSpecialClients(EmployeeType type, string notification)
         {
 
             StringBuilder messageToLog = new StringBuilder();
@@ -76,7 +75,7 @@ namespace HiringCompany
                 {
                     var c = from x in access.employees
                             where x.Type == type // special type
-                            select ( x.Username );
+                            select (x.Username);
 
                     clients = c.ToList();
                 }
@@ -86,27 +85,34 @@ namespace HiringCompany
                 foreach (var clientUsername in clients)
                 {
 
-                    var clientChannel = hiringCompanyDB.ConnectionChannelsClients[clientUsername];
-                    ICommunicationObject cObject = clientChannel as ICommunicationObject;
-                    if (cObject != null)
+                   IEmployeeServiceCallback clientChannel;
+                    if (hiringCompanyDB.ConnectionChannelsClients.TryGetValue(clientUsername, out clientChannel))
+                    // klijent povezan sa serverom
                     {
-                        if (cObject.State == CommunicationState.Opened)
+                        ICommunicationObject cObject = clientChannel as ICommunicationObject;
+                        if (cObject != null)
                         {
-                            clientChannel.SyncData(cData);
-
-                            if (!notification.Equals(string.Empty))
+                            if (cObject.State == CommunicationState.Opened)
                             {
-                                clientChannel.Notify(notification);
-                            }
+                                clientChannel.SyncData(cData);
 
-                        }
-                        else
-                        {
-                            forRemove.Add(clientUsername);
-                            messageToLog.AppendLine(string.Format("SyncData() failed for client {0}: because clientChannel was in {1} state.", clientUsername, cObject.State.ToString()));
+                                if (!notification.Equals(string.Empty))
+                                {
+                                    clientChannel.Notify(notification);
+                                }
+
+                            }
+                            else
+                            {
+                                forRemove.Add(clientUsername);
+                                messageToLog.AppendLine(string.Format("SyncData() failed for client {0}: because clientChannel was in {1} state.", clientUsername, cObject.State.ToString()));
+                            }
                         }
                     }
+                    else
+                    { // treba cuvati notifikaciju u bazi
 
+                    }
                 }
 
                 foreach (string key in forRemove)
@@ -145,7 +151,92 @@ namespace HiringCompany
             Program.Logger.Info(messageToLog);
         }
 
-        public void NotifySpecialClient( string clientUsername, string notification )
+        public void NotifySpecialClients(EmployeeType type, string notification)
+        {
+            StringBuilder messageToLog = new StringBuilder();
+            messageToLog.AppendLine(string.Format("Method: Notifier.NotifySpecialClients()")); // dodati params, srediti loger i to
+
+            PopulateCurrentData();
+
+            try
+            {
+                List<string> clients;
+                using (var access = new AccessDB())
+                {
+                    var c = from x in access.employees
+                            where x.Type == type // special type
+                            select (x.Username);
+
+                    clients = c.ToList();
+                }
+
+                List<string> forRemove = new List<string>();
+
+                foreach (var clientUsername in clients)
+                {
+
+                    IEmployeeServiceCallback clientChannel;
+                    if (hiringCompanyDB.ConnectionChannelsClients.TryGetValue(clientUsername, out clientChannel))
+                    // klijent povezan sa serverom
+                    {
+                        ICommunicationObject cObject = clientChannel as ICommunicationObject;
+                        if (cObject != null)
+                        {
+                            if (cObject.State == CommunicationState.Opened)
+                            {
+                                    clientChannel.Notify(notification);                               
+                            }
+                            else
+                            {
+                                forRemove.Add(clientUsername);
+                                messageToLog.AppendLine(string.Format("SyncData() failed for client {0}: because clientChannel was in {1} state.", clientUsername, cObject.State.ToString()));
+                            }
+                        }
+                    }
+                    else
+                    { // treba cuvati notifikaciju u bazi
+
+                    }
+                }
+
+                foreach (string key in forRemove)
+                {
+                    hiringCompanyDB.ConnectionChannelsClients.Remove(key);
+                    messageToLog.AppendLine(string.Format("removed channel with key {0}", key));
+                }
+
+                messageToLog.AppendLine("finished successfully");
+                Program.Logger.Info(messageToLog);
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    messageToLog.AppendLine(string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        messageToLog.AppendLine(string.Format("- Property: \"{0}\", Value: \"{1}\", Error: \"{2}\"",
+                            ve.PropertyName,
+                            eve.Entry.CurrentValues.GetValue<object>(ve.PropertyName),
+                            ve.ErrorMessage));
+                    }
+                }
+            }
+            catch (Exception e) // videti sta je ovde bilo greska
+            {
+                Console.WriteLine(e);
+
+            }
+
+
+            messageToLog.AppendLine("finished successfully");
+            Program.Logger.Info(messageToLog);
+
+
+        }
+        public void NotifySpecialClient(string clientUsername, string notification)
         {
             // probati da izvuces kanal, ako ga ima poslati notifiakciju, ako je nema, sacuvati je u bazi i poslati je kad se korisnik prijavi..
             // kas se korisniku posalju notifikacije, onda mozemo da ih izbrisemo iz baze?
@@ -170,7 +261,7 @@ namespace HiringCompany
             else // cuvaj u bazi, napraviti metodu u ovom notifieru koja se poziva iz sign in, koaj ce citati sve notifikacije iz baze i slati klijentu
             {
 
-            }    
+            }
 
         }
 
