17f3cb1e69baf42f24833b2c3a12f90649cedda8
Zvezdana94 <zklasnic94@gmail.com>
2017-01-16 11:15:55 +0100

    Mod1
    Reports

---
+++ Moduo1/EmployeeCommonTest/DataTest/ExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeCommon;
+using EmployeeCommon.Data;
+
+namespace EmployeeCommonTest.DataTest
+{
+    [TestFixture]
+    public class ExtensionsTest
+    {
+        [Test]
+        [TestCase(EmployeeType.CEO, "Chief Executive Officer")]
+        [TestCase(EmployeeType.HR, "Human Resources")]
+        [TestCase(EmployeeType.PO, "Product Owner")]
+        [TestCase(EmployeeType.SM, "Scrum Master")]
+        public void TypeToStringTestOk(EmployeeType type,string pos)
+        {
+            string position = Extensions.TypeToString(type);
+            Assert.AreEqual(position, pos);
+        }
+
+        [Test]
+        [TestCase(EmployeeType.CEO, "Chief Executive Officer")]
+        [TestCase(EmployeeType.HR, "Human Resources")]
+        [TestCase(EmployeeType.PO, "Product Owner")]
+        [TestCase(EmployeeType.SM, "Scrum Master")]
+        [TestCase(EmployeeType.CEO,"bla")]
+        public void StringToType(EmployeeType type, string pos)
+        {
+            EmployeeType t = Extensions.StringToType(pos);
+            Assert.AreEqual(t, type);
+        }
+    }
+}
